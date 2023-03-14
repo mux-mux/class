203834a5009c85f5e88ffe7f17208e620a8c11cc
mikemouse <kmmyemail@ukr.net>
2023-03-14 14:05:16 +0200

    feat: create Cards trough Class

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,11 @@ window.addEventListener('DOMContentLoaded', () => {
     tabContent = document.querySelectorAll('.tabcontent');
 
   function hideTabContent() {
-    tabs.forEach(tab => {
+    tabs.forEach((tab) => {
       tab.classList.remove('tabheader__item_active');
     });
 
-    tabContent.forEach(content => {
+    tabContent.forEach((content) => {
       content.classList.add('sidepanel__hide');
       content.classList.remove('sidepanel__show');
     });
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabContent[i].classList.remove('sidepanel__hide');
     tabContent[i].classList.add('sidepanel__show');
-  };
+  }
 
   hideTabContent();
   showTabContent();
@@ -43,17 +43,17 @@ window.addEventListener('DOMContentLoaded', () => {
   function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date()),
       days = Math.floor(total / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((total / (1000 * 60 * 60) % 24)),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24),
       minutes = Math.floor((total / 1000 / 60) % 60),
       seconds = Math.floor((total / 1000) % 60);
 
     return {
-      'total': total,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    }
+      total: total,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
   }
 
   function getZero(num) {
@@ -103,4 +103,67 @@ window.addEventListener('DOMContentLoaded', () => {
     modal.classList.add('sidepanel__hide');
     modal.classList.remove('sidepanel__show');
   });
+
+  //Class for Cards
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+      <div class="menu__item">
+        <img src="${this.src}" alt="${this.alt}">
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      </div>
+    `;
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    21,
+    '.menu .container'
+  ).render();
 });
